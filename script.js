a456f1ffc53d4bb9952d1d2613908f5478c9bebd
Sander-Kode <sanderellaman@gmail.com>
2025-11-27 13:42:57 +0100

    lagt til js

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log ("hello world", 1234, true);
+
+/*
+datatyper:
+- string = tekst " "
+- number = tall 
+- boolean = true/false
+*/
+
+/*
+variabler:
+- const = konstant variabel som ikke kan bytte verdi
+- let = variabel som kan bytte verdi
+- var = utdatert variabel som kan bytte verdi men som IKKE skal brukes
+*/
+
+const greeting = "God morgen";
+// gretting = "God ettermiddag"; om man hadde brukt "let" istedenfor const//
+console.log(greeting);
+const userName = "Sander";
+console.log(userName);
+const time = 1044;
+console.log(time);
+const isPresent = true;
+console.log(isPresent);
+
